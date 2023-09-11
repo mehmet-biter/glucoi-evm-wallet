8a1beb593bb9747417a3af93c78d3d0bcd15b35b
fil303 <webfil303@gmail.com>
2023-09-11 19:26:07 +0600

    update

--- src/routes/v1/wallet.route.ts
+++ src/routes/v1/wallet.route.ts
@@ -7,5 +7,8 @@ import evmValidation from "../../validations/wallet.validation";
 const router = express.Router();
 
 router.post("/create-wallet", auth(), validate(evmValidation.walletCreate), evmController.createWallet);
+router.get("/test", (req, res, next) => {
+    return res.send({success:200})
+});
 
 export default router;
--- src/services/wallet.service.ts
+++ src/services/wallet.service.ts
@@ -6,40 +6,56 @@ import createEthAddress from "./web3.service";
 const prisma = new PrismaClient();
 
 const createAddress = async (user:any,coinType: any, network: number) => {
-  console.log(user);
-
-    const getNetwork = await prisma.networks.findFirst({
-        where: {
-            id: network,
-            status: STATUS_ACTIVE
-        }
-    });
-    
+  const User = user.user_details;
+  const getNetwork = await prisma.networks.findUnique({
+    where : {
+      id : network
+    }
+  });
+  
   if (getNetwork) {
     let wallet = generateErrorResponse("Invalid base type");
     if (getNetwork.base_type == EVM_BASE_COIN) {
-        // wallet = await createEthAddress(coinType, network);
-          // if (wallet) {
-          //     return generateSuccessResponse("Wallet created successfully",wallet);
-          //   } else {
-          //     return generateErrorResponse("Wallet not generated");
-          //   }
+        wallet = await createEthAddress(coinType, network);
+          if(await createWalletAddressHistorie(Number(User.id), coinType, Number(getNetwork.id), wallet)) {
+            return generateSuccessResponse("Wallet created successfully",wallet.data.address);
+          } else {
+            return generateErrorResponse("Wallet not generated");
+          }
     } else {
       wallet = generateErrorResponse("Invalid base type");
     }
-    if (wallet.success) {
-      // const userWallet = await prisma.wallets.findFirst({
-      //   where: {
-      //     user_id: user.id,
-      //     coin_type: coinType
-      //   }
-      // });
 
-    }
   } else {
     return generateErrorResponse("Network not found");
   } 
+  return generateErrorResponse("Network not found"); 
 };
+
+const createWalletAddressHistorie = async (userId:number, coinType:string, networkId:number, wallet:any) => {
+  if(wallet?.success){
+    const userWallet = await prisma.wallets.findFirst({
+      where: {
+        user_id: userId,
+        coin_type: coinType
+      }
+    });
+
+    const walletAddress = await prisma.wallet_address_histories.create({
+      data : {
+        user_id : userId,
+        coin_type : coinType,
+        network_id : networkId,
+        wallet_id : Number(userWallet?.id),
+        wallet_key : wallet.data.privateKey,
+        address : wallet.data.address,
+      }
+    });
+    if(walletAddress) return true;
+    return false;
+  } 
+  return false;
+}
 
 export default (
     createAddress
