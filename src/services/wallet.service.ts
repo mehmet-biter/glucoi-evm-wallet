b6467857aa4c1931ba6f6b9887084e2f6004b5a5
fil303 <webfil303@gmail.com>
2023-09-12 12:09:21 +0600

    address check in db

--- src/services/wallet.service.ts
+++ src/services/wallet.service.ts
@@ -12,12 +12,30 @@ const createAddress = async (user:any,coinType: any, network: number) => {
       id : network
     }
   });
-  
+
+  const userWallet = await prisma.wallets.findFirst({
+    where: {
+      user_id: Number(User.id),
+      coin_type: coinType
+    }
+  });
+  if(!userWallet) return generateErrorResponse("Wallet not found");
+
+  const walletAddress = await prisma.wallet_address_histories.findFirst({
+    where: {
+      user_id : Number(User.id),
+      coin_type : coinType,
+      network_id : Number(getNetwork?.id),
+      wallet_id : Number(userWallet?.id),
+    }
+  });
+  if(walletAddress) return generateSuccessResponse("Wallet address found successfully", walletAddress.address);
+
   if (getNetwork) {
     let wallet = generateErrorResponse("Invalid base type");
     if (getNetwork.base_type == EVM_BASE_COIN) {
         wallet = await createEthAddress(coinType, network);
-          if(await createWalletAddressHistorie(Number(User.id), coinType, Number(getNetwork.id), wallet)) {
+          if(await createWalletAddressHistorie(Number(User.id), coinType, Number(getNetwork.id), wallet, userWallet)) {
             return generateSuccessResponse("Wallet created successfully",wallet.data.address);
           } else {
             return generateErrorResponse("Wallet not generated");
@@ -29,15 +47,8 @@ const createAddress = async (user:any,coinType: any, network: number) => {
   return generateErrorResponse("Network not found"); 
 };
 
-const createWalletAddressHistorie = async (userId:number, coinType:string, networkId:number, wallet:any) => {
+const createWalletAddressHistorie = async (userId:number, coinType:string, networkId:number, wallet:any, userWallet:any) => {
   if(wallet?.success){
-    const userWallet = await prisma.wallets.findFirst({
-      where: {
-        user_id: userId,
-        coin_type: coinType
-      }
-    });
-
     const walletAddress = await prisma.wallet_address_histories.create({
       data : {
         user_id : userId,
