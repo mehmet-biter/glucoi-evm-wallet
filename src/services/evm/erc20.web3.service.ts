e871f4ffce93ab492320f9fe1a99720f9af1612a
ubuntu-pc <bacchu.cse11@gmail.com>
2023-09-18 15:41:50 +0600

    erc20 web3 modified

--- src/services/evm/erc20.web3.service.ts
+++ src/services/evm/erc20.web3.service.ts
@@ -3,28 +3,20 @@ import Web3 from "web3";
 import { EVM_BASE_COIN, STATUS_ACTIVE } from "../../utils/coreConstant";
 import { generateErrorResponse, generateSuccessResponse } from "../../utils/commonObject";
 import { ERC20_ABI } from "../../contract/erc20.token.abi";
-import { customFromWei } from "../../utils/helper";
+import { addNumbers, convertCoinAmountFromInt, convertCoinAmountToInt, customFromWei, minusNumbers, multiplyNumbers } from "../../utils/helper";
+import { TransactionConfig } from 'web3-core';
+
 
 const prisma = new PrismaClient();
 
-const initializeWeb3 = async (network:number) => {
-  let rpcUrl = "";
-  const coin: any = await prisma.$queryRaw`
-    select * from coin_networks 
-    join networks on networks.id = coin_networks.network_id
-    join supported_network on networks.slug = supported_network.slug
-    where networks.id = ${network} limit 1
-    `;
-  
-    if (coin && coin.length > 0) {
-        rpcUrl = coin[0].rpc_url;
-    }
-   
+// initialize web3
+const initializeWeb3 = async (rpcUrl:string) => { 
    const connectWeb3 = new Web3(new Web3.providers.HttpProvider(rpcUrl));
   
   return connectWeb3;
 };
 
+// initialize contract
 const initializeErc20Contact = async (web3:any, contractAddress:string) => {
   const tokenContract = new (await web3).eth.Contract(JSON.parse(ERC20_ABI), contractAddress);
   return tokenContract;
@@ -35,9 +27,10 @@ const contractDecimal = async (tokenContract:any) => {
   return await tokenContract.methods.decimals().call();
 }
 
-export const createEthAddress = async (coinType: string, network: number) => {
+// create eth address
+const createEthAddress = async (coinType: string, rpcUrl: string) => {
   try {
-    const connectWeb3: any = await initializeWeb3(network);
+    const connectWeb3: any = await initializeWeb3(rpcUrl);
     let wallet = await connectWeb3.eth.accounts.create();
     if (wallet) {
       return generateSuccessResponse("Wallet created successfully", wallet);
@@ -50,9 +43,10 @@ export const createEthAddress = async (coinType: string, network: number) => {
   }
 };
 
-export const getEthBalance = async (coinType: string, network: number, address:string) => {
+// get eth balance
+const getEthBalance = async (rpcUrl: string, address:string) => {
   try {
-    const connectWeb3: any = await initializeWeb3(network);
+    const connectWeb3: any = await initializeWeb3(rpcUrl);
   
     let balance = '0';
     const netBalance = await connectWeb3.eth.getBalance(address);
@@ -68,9 +62,10 @@ export const getEthBalance = async (coinType: string, network: number, address:s
   }
 }
 
-export const getEthTokenBalance = async (coinType: string, network: number, address:string, contractAddress:string) => {
+// get eth token balance
+const getEthTokenBalance = async (rpcUrl: string, address:string, contractAddress:string) => {
   try {
-    const connectWeb3: any = await initializeWeb3( network);
+    const connectWeb3: any = await initializeWeb3(rpcUrl);
     const tokenContract = await initializeErc20Contact(connectWeb3, contractAddress)
     let balance:any = 0;
     const tokenBalance = await tokenContract.methods.balanceOf(address).call();
@@ -86,6 +81,88 @@ export const getEthTokenBalance = async (coinType: string, network: number, addr
     return generateErrorResponse("Something went wrong");
   }
 }
+
+// get estimate fees for eth
+const estimateEthFee = async (
+  rpcUrl: string, 
+  coinType:string, 
+  coinDecimal:any, 
+  gasLimit:any, 
+  fromAddress:string, 
+  toAddress:string,
+  amount:any
+  ) => {
+  try {
+    const connectWeb3 = await initializeWeb3(rpcUrl);
+    const gasPrice = await connectWeb3.eth.getGasPrice();
+
+    let message = '';
+
+    const tx: TransactionConfig = {
+      from: connectWeb3.utils.toChecksumAddress(fromAddress),
+      to: connectWeb3.utils.toChecksumAddress(toAddress),
+      value: convertCoinAmountToInt(amount,coinDecimal),
+      gasPrice: gasPrice.toString(),
+      gas: gasLimit.toString()
+    };
+
+    const maxFee = Number(convertCoinAmountToInt(
+      multiplyNumbers(gasLimit,Number(gasPrice)),coinDecimal,)
+    );
+
+    const balanceRequired = addNumbers(Number(maxFee),amount);
+    const balance = await getEthBalance(rpcUrl,fromAddress);
+
+    if (Number(balanceRequired) > Number(balance)) {
+      const balanceShortage = minusNumbers(
+        Number(balanceRequired),
+        Number(balance),
+      );
+      message = `${'Insufficient '} ${coinType} ${
+        'balance including fee'}!!\n
+       ${'balance required'}: ${balanceRequired.toFixed(
+        10,
+      )} ${coinType},\n
+       ${'balance exists'}: ${balance} ${coinType},\n
+       ${'balance shortage'}: ${balanceShortage.toFixed(
+        12,
+      )} ${coinType}.\n
+       ${'Try less amount.'}`;
+      console.log(message);
+      // console.log('\n');
+      return generateErrorResponse(message);
+    }
+
+    const gas = await connectWeb3.eth.estimateGas(tx);
+
+    if (gas > gasLimit) {
+      message = `Network is too busy now, Fee is too high. ${
+        'Sending'
+      } ${coinType} ${'coin .'}
+      ${'it will ran out of gas. gas needed'}=${gas}, ${
+        'gas limit we are sending'}=${gasLimit}`;
+      // console.log(message);
+      // console.log('\n');
+      return generateErrorResponse(message);
+    }
+
+    const estimatedFee = Number(
+      convertCoinAmountFromInt(
+        multiplyNumbers(gas, Number(gasPrice)),
+        coinDecimal,
+      ),
+    );
+
+    return generateSuccessResponse('success', {
+      fee:estimateEthFee
+    })
+
+  } catch( err ) {
+    console.log(err);
+    return generateErrorResponse("Something went wrong");
+  }
+} 
+
 
 export const createEvmAddress = async (rpcUrl: string|null) => {
   rpcUrl = rpcUrl || "/";
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,5 @@
+import { Decimal } from "@prisma/client/runtime";
+import * as safeMath from '@dip1059/safe-math-js';
 
 export const powerOfTen = (x:any) => {
     return Math.pow(10,x);
@@ -6,3 +8,48 @@ export const powerOfTen = (x:any) => {
 export const customFromWei = async(amount:any,decimal:any) => {
     return (amount/powerOfTen(decimal)).toString();
 }
+
+export const convertCoinAmountToInt = (
+    amount: Decimal | number,
+    decimal: number,
+  ): string => {
+    let multiplier: any = '1';
+    for (let i = 0; i < decimal; i++) {
+      multiplier += '0';
+    }
+    const result = multiplyNumbers(Number(amount), Number(multiplier));
+    return result['noExponents']().split('.')[0];
+  }
+
+export const convertCoinAmountFromInt = (
+    amount: string | number,
+    decimal: number,
+  ): string => {
+    let multiplier: any = '1';
+    for (let i = 0; i < decimal; i++) {
+      multiplier += '0';
+    }
+    const result = divideNumbers(Number(amount), Number(multiplier));
+    return result['noExponents']();
+  }
+
+
+  export const addNumbers = (...numbers: number[]): number  => {
+    return safeMath.add(numbers);
+  }
+  
+  export const minusNumbers =(...numbers: number[]): number => {
+    return safeMath.minus(numbers);
+  }
+  
+  export const multiplyNumbers = (...numbers: number[]): number => {
+    return safeMath.multiply(numbers);
+  }
+  
+  export const divideNumbers = (...numbers: number[]): number => {
+    return safeMath.divide(numbers);
+  }
+  
+  export const formatAmountDecimal = (amount: number, decimal: number): number  =>{
+    return Number(amount.toFixed(decimal));
+  }
